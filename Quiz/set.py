3a35f695afe22c38a71bcbffd4eaa4b3379e3b9f
FeelingXD <wlals425315@gmail.com>
2020-06-15 15:38:47 +0900

    6-15

---
+++ Quiz/set.py
@@ -0,0 +1,26 @@
+#set 자료형  집합 ex  중복이 안되고 , 순서가 없음
+
+my_set={1,2,3,3,3}
+print(my_set)
+
+java ={'유재석','조세호','박명수'}
+python ={'유재석','이명수'}
+
+# 교집함 출력하기
+print(java & python)  # and 로는 사용이 안된다. :/
+print(java.intersection(python)) # 동일한 기능을한다.
+
+# 합집합
+print(java | python) # java 혹은 python
+print(java.union(python)) # 동일한 기능을 한다.
+
+# 차집합
+print( java- python)
+print(java.difference(python))
+
+# 추가하기
+java.add('김태호')
+print(java)
+
+java.remove('김태호')
+print(java)
---
+++ Quiz/tuple.py
@@ -0,0 +1,6 @@
+#tuple자료형 리스트와 유사하지만 읽기전용이다.
+menu = ("돈까스","치즈")
+print(menu)
+
+print(menu[0])
+print(menu[1])# 리스트 목록처럼 사용할수도있다.
