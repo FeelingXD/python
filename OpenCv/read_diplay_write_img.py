ed83e729bfff65ca37b82341b78bd03e4b859d1b
FeelingXD <wlals425315@gmail.com>
2020-06-28 03:35:24 +0900

    open cv 28

--- OpenCv/read_diplay_write_img.py
+++ OpenCv/read_diplay_write_img.py
@@ -6,6 +6,10 @@
 import cv2 as cv
 import sys
 img = cv.imread("C:\\Users\\ghpro\\Pictures\\Saved Pictures\\center_article_img.jpg")
+# cv.imread (경로,flag) falg default=color options =
+# cv2.IMREAD_COLOR : 이미지 파일을 Color로 읽어들입니다. 투명한 부분은 무시되며, Default값입니다.
+# cv2.IMREAD_GRAYSCALE : 이미지를 Grayscale로 읽어 들입니다. 실제 이미지 처리시 중간단계로 많이 사용합니다.
+# cv2.IMREAD_UNCHANGED : 이미지파일을 alpha channel까지 포함하여 읽어 들입니다.
 if img is None:
     sys.exit("Could not read the image.")
 cv.imshow("Display window", img)
