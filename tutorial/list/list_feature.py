526589a168c62df46f510e905ff6958b299e1a98
FeelingXD <wlals425315@gmail.com>
2020-07-08 04:09:07 +0900

    7-8

---
+++ tutorial/list/list_feature.py
@@ -0,0 +1,11 @@
+'''
+list.index()
+list.extend()
+list.insert()
+list.revers()
+list.sort()
+list.join()
+'''
+list1 = ['a','b','c']
+print(list1.index('b')) #index()해당원소의 인데스 값을가져옴
+print('^^'.join(list1))# join 중앙에 추가문자열
