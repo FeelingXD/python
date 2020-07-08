690b43ea8a895d0a89389fc7ad3437101c5dc769
FeelingXD <wlals425315@gmail.com>
2020-07-09 04:08:01 +0900

    7-9

--- beakjoon/p10871.py
+++ beakjoon/p10871.py
@@ -1,8 +1,8 @@
 import sys
 a,b =map(int,sys.stdin.readline().split())#a 수열갯수 ,b 는 기준값
-numlist= list(int(input().split()))
+numlist= list(input().split())
 resultlist=[]
 for i in range(a):
-    if numlist[i]<b:
+    if int(numlist[i])<b:
         resultlist.append(numlist[i])
 print(' '.join(resultlist))
