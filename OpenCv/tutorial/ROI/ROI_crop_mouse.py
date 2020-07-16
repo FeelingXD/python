ba8cfc426e38a4dfdbd506460565da791220058e
FeelingXD <wlals425315@gmail.com>
2020-07-16 19:46:00 +0900

    7-16

--- OpenCv/tutorial/ROI/ROI_crop_mouse.py
+++ OpenCv/tutorial/ROI/ROI_crop_mouse.py
@@ -6,29 +6,32 @@ x0,y0,w,h=-1,-1,-1,-1#영역 선택 좌표
 blue,red = (255,0,0),(0,0,255)#생상값
 
 def onMouse(event,x,y,flags,param):#마우스이밴트 핸들
-    global isDragging, x0, y0, img
+    global isDragging, x0, y0, img #전역변수 참조
+
     if event==cv.EVENT_LBUTTONDOWN:
         isDragging=True
         x0=x
         y0=y
+
     elif event == cv.EVENT_MOUSEMOVE:
         if isDragging:
             img_draw =img.copy()
             cv.rectangle(img_draw,(x0,y0),(x,y),blue,2)#드래그영역
             cv.imshow('img',img_draw)
+
     elif event == cv.EVENT_LBUTTONUP:
         if isDragging:
-            isDragging:False
+            isDragging = False
             w=x-x0
             h=y-y0
             print('x:%d, y:%d, w:%d, h:%d'% (x0,y0,w,h))
             if w>0 and h>0:
                 img_draw = img.copy()
-
                 cv.rectangle(img_draw,(x0,y0), (x,y), red ,2)
                 cv.imshow('img',img_draw)
-                roi = img[y:y0+h,x0:x0+w]
-                cv.imshow('cropped',roi)
+                roi = img[y:y0+h, x0:x0+w]
+                cv.imshow('null', roi)
+
                 cv.moveWindow('cropped',0,0)
                 cv.imwrite('../../img/cropped.png',roi)
                 print("cropped.")
--- OpenCv/tutorial/ROI/ROIset.py
+++ OpenCv/tutorial/ROI/ROIset.py
@@ -10,22 +10,25 @@ start_x, start_y=-1,-1
 def mouse_callback(event,x,y,flags,param):
     global start_x,start_y,mouse_is_pressing
     img_result =img_color.copy()
+
     if event == cv.EVENT_LBUTTONDOWN:
         mouse_is_pressing=True
         start_x,start_y=x,y
         cv.circle(img_result,(x,y),10,(0,255,0),-1)
         cv.imshow("img_color",img_result)
+
     elif event == cv.EVENT_MOUSEMOVE:
         if mouse_is_pressing:
             cv.rectangle(img_result,(start_x,start_y),(x,y),(0,255,0),3)
             cv.imshow("img_color",img_result)
+
     elif event == cv.EVENT_LBUTTONUP:
         mouse_is_pressing=False
-        img_cat = img_color[start_y:y,start_x:x]
-        img_cat = cv.cvtColor(img_cat,cv.COLOR_BGR2GRAY)
-        img_cat = cv.cvtColor(img_cat,cv.COLOR_GRAY2BGR)
+        img_land = img_color[start_y:y,start_x:x]
+        img_land = cv.cvtColor(img_land,cv.COLOR_BGR2GRAY)
+        img_land = cv.cvtColor(img_land,cv.COLOR_GRAY2BGR)
         cv.imshow("img_color", img_result)
-        cv.imshow("img_cat",img_cat)
+        cv.imshow("img_land",img_land)
 
 img_color = cv.imread('../../img/solidWhiteCurve.jpg',cv.IMREAD_COLOR)
 cv.imshow('img_color',img_color)
