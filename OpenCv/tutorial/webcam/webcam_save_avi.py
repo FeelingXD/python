9a2fdd2cac992a32bb31d8b41dac9dde4c335d1f
FeelingXD <wlals425315@gmail.com>
2020-07-21 21:21:38 +0900

    7-21(save avi from webcam

---
+++ OpenCv/tutorial/webcam/webcam_save_avi.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+fourcc =cv.VideoWriter_fourcc(*'XVID')
+
+#컬러
+writer_c = cv.VideoWriter('output.avi',fourcc,30.0,(width,height))
+#그래이
+# writer_g = cv.VideoWriter('output.avi',fourcc,30.0,(width,height),0)
+
+while True:
+    ret,img = cap.read()
+
+    if ret ==False:
+        break
+
+    img_gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+    cv.imshow('color',img)
+    cv.imshow('gray',img_gray)
+
+    writer_c.write(img)
+
+    if cv.waitKey(1)&0xff == 27:
+        break
+
+cap.release()
+writer_c.release()
+cv.destroyAllWindows()
